92689376a7799127438c66bddf5570bb40115f5f
wjdwo951219 <wjdwo951219@gmail.com>
2025-11-24 09:17:27 +0900

    jsp project init

--- src/main/java/kr/co/pionnet/scdev4/bot/Scdev4BotApplication.java
+++ src/main/java/kr/co/pionnet/scdev4/bot/Scdev4BotApplication.java
@@ -2,22 +2,12 @@ package kr.co.pionnet.scdev4.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import java.time.ZoneId;
-import java.util.TimeZone;
+@SpringBootApplication
+public class Scdev4BotApplication {
 
-@SpringBootApplication(scanBasePackages = "kr.co.pionnet.scdev4.bot")
-public class Scdev4BotApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Scdev4BotApplication.class);
-	}
-	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("GMT+9")));
-		SpringApplication.run(Scdev4BotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Scdev4BotApplication.class, args);
+    }
 
 }
