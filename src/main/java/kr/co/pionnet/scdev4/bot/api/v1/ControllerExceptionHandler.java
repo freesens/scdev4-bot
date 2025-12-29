a60ba84ebc6d130f6b18bb72d10c9f98defe19e9
최우빈 <3jdnqls@naver.com>
2025-12-29 15:30:27 +0900

    IllegalStateException 예외처리 수정

--- src/main/java/kr/co/pionnet/scdev4/bot/api/v1/ControllerExceptionHandler.java
+++ src/main/java/kr/co/pionnet/scdev4/bot/api/v1/ControllerExceptionHandler.java
@@ -5,18 +5,28 @@ import kr.co.pionnet.scdev4.bot.domain.common.dto.v1.ResponseV1;
 import kr.co.pionnet.scdev4.bot.domain.common.entity.ApiResultEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseV1<?> handleException(IllegalStateException e) {
+    public ResponseEntity<Map<String, Object>> handleException(IllegalStateException e) {
         log.error(e.getMessage(), e);
 
-        return ResponseV1.builder(ApiResultEnum.SUCCESS.getCode(), NoticeMessage.clientMessage.ALREADY_CLICKED).build();
+        Map<String, Object> result = new HashMap<>();
+        result.put("result", "Fail");
+        result.put("failMessage", NoticeMessage.clientMessage.ALREADY_CLICKED);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(result);
     }
 
     @ExceptionHandler(value = {Exception.class})
